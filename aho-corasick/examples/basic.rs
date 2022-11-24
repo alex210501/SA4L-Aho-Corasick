c726d911691da59b9b0d88d66bb1ebaf1137e425
Alejandro <alejandrobrb210501@gmail.com>
2022-11-24 16:37:49 +0100

    (#8) Add basic example

---
+++ aho-corasick/examples/basic.rs
@@ -0,0 +1,22 @@
+/*
+    Example based on the value of the YouTube video:
+    https://youtu.be/O7_w001f58c
+*/
+
+use aho_corasick::aho_corasick::AhoCorasick;
+
+fn main() {
+    let patterns: Vec<String> = vec![
+        String::from("acc"),
+        String::from("atc"),
+        String::from("cat"),
+        String::from("gcg"),
+    ];
+    let text = String::from("gcatcg");
+
+    let mut ac = AhoCorasick::new(&patterns);
+    ac.search_text(&text);
+
+    println!("Search in \"{}\" the followings patterns {:?}", text, patterns);
+    println!("Results: {:?}", ac.result.clone());
+}
