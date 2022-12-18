640e62116086fb76ee0e065b10d6e3839ae0dfea
Alex <alejandrobrb210501@gmail.com>
2022-12-18 23:20:42 +0100

    Fix bug in html-tag example

--- aho-corasick/examples/html-tag/main.rs
+++ aho-corasick/examples/html-tag/main.rs
@@ -1,5 +1,5 @@
-use std::{env, fs, io::Write};
 use std::fs::File;
+use std::{env, fs, io::Write};
 
 use aho_corasick::aho_corasick::AhoCorasick;
 
@@ -15,24 +15,28 @@ fn main() {
 
     let args = Vec::from_iter(env::args());
     let patterns: Vec<&str> = args[1..].iter().map(|v| v.as_ref()).collect::<Vec<_>>();
-    
+
     let mut ac = AhoCorasick::new(&patterns);
     ac.search_text(&contents);
 
     let mut last_pattern: usize = 0;
 
     output.push_str(OPEN_BODY);
-    for match_result in ac.result.iter() {
-        output.push_str(&contents[last_pattern..match_result.start as usize]);
-        output.push_str(OPEN_TAG);
-        output.push_str(&contents[match_result.start as usize..match_result.end as usize + 1]);
-        output.push_str(CLOSE_TAG);
-        last_pattern = match_result.end as usize + 1;
+    if ac.result.is_empty() {
+        output.push_str(&contents);
+    } else {
+        for match_result in ac.result.iter() {
+            output.push_str(&contents[last_pattern..match_result.start as usize]);
+            output.push_str(OPEN_TAG);
+            output.push_str(&contents[match_result.start as usize..match_result.end as usize + 1]);
+            output.push_str(CLOSE_TAG);
+            last_pattern = match_result.end as usize + 1;
+        }
     }
     output.push_str(CLOSE_BODY);
-    
+
     let _ = match File::create("./examples/html-tag/output.html") {
-        Ok(mut file) =>  file.write_all(output.as_bytes()),
+        Ok(mut file) => file.write_all(output.as_bytes()),
         Err(err) => Err(err),
     };
 }
